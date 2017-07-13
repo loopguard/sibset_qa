fad35f20bc53e0246dae03df6f18b721fd0ec978
Alexander Efimov <fonwolfgang@gmail.com>
2017-07-13 10:35:07 +0700

    Fixed

--- Tests/norcom_test.py
+++ Tests/norcom_test.py
@@ -2,7 +2,7 @@
 # from selenium.webdriver.common.action_chains import ActionChains
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-# from selenium.webdriver.common.by import By
+from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome('C:\Tools\chromedriver')
 driver.get('http://beta.norcom.ru/')
