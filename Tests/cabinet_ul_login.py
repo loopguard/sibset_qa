ab567f0915c695ccc32067d14bb963d46d1a159b
Alexander Efimov <fonwolfgang@gmail.com>
2017-07-13 10:38:22 +0700

    Fix
    Добавлено описание

--- Tests/cabinet_ul_login.py
+++ Tests/cabinet_ul_login.py
@@ -1,18 +1,11 @@
-import pytest
+#Вход в личный кабинет юр.лица с вводом корректных данных
+
+
 from selenium import webdriver
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
 
 driver = webdriver.Chrome() #расположение chromedriver можно указать прямо 'C:\Tools\chromedriver'
 driver.get('http://cabinet-ul.sibset.ru/login')
 driver.find_element_by_name('login').send_keys('212140Tk')
 driver.find_element_by_name('password').send_keys('212140Tk')
-driver.find_element_by_class_name('ss-button').click() #кнопка войти
-
-
-
-#WebDriverWait(driver, 10).until(EC.title_is("Наименование организации"))
-
-
-#driver.close()
+driver.find_element_by_class_name('ss-button').click()
