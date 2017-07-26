afd71143240f85b11c564a76f35c5f03526c2fa1
Alexander Efimov <fonwolfgang@gmail.com>
2017-07-26 16:42:46 +0700

    Добавил раздел "настройки"
    Ran 8 tests in 48.039s
    
    OK

--- Tests/ul_sibset_login_test.py
+++ Tests/ul_sibset_login_test.py
@@ -27,7 +27,6 @@ class login_ul_chrome_test(unittest.TestCase): #Chromedriver
         wait = WebDriverWait(driver, 10)
         wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="root"]/div/div/div/div[4]/div/div[1]/div[2]/a[1]')))
 
-
     def test_2_logout(self): #выход из личного кабинета
         driver = self.driver
         driver.find_element_by_name('login').send_keys('212140Tk')
@@ -44,6 +43,17 @@ class login_ul_chrome_test(unittest.TestCase): #Chromedriver
         driver.find_element_by_class_name('ss-button').click()
         wait = WebDriverWait(driver, 10)
         wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'error')))
+
+    def test_4_settings(self): #раздел настройки, смена пароля
+        driver = self.driver
+        driver.find_element_by_name('login').send_keys('212140Tk')
+        driver.find_element_by_name('password').send_keys('212140Tk')
+        driver.find_element_by_class_name('ss-button').click()
+        wait = WebDriverWait(driver, 10)
+        driver.implicitly_wait(10)
+        driver.find_element_by_css_selector('#root > div > div > div > header > div.additional_menu > a:nth-child(1)').click()
+        wait.until(EC.presence_of_element_located((By.NAME, 'old_password')))
+
 
 
 class login_ul_firefox_test(unittest.TestCase): #Firefox
@@ -73,6 +83,16 @@ class login_ul_firefox_test(unittest.TestCase): #Firefox
         driver.find_element_by_name('login').send_keys('1234')
         driver.find_element_by_name('password').send_keys('#$^')
         driver.find_element_by_class_name('ss-button').click()
+
+    def test_4_settings(self): #раздел настройки, смена пароля
+        driver = self.driver
+        driver.find_element_by_name('login').send_keys('212140Tk')
+        driver.find_element_by_name('password').send_keys('212140Tk')
+        driver.find_element_by_class_name('ss-button').click()
+        wait = WebDriverWait(driver, 10)
+        driver.implicitly_wait(10)
+        driver.find_element_by_css_selector('#root > div > div > div > header > div.additional_menu > a:nth-child(1)').click()
+        wait.until(EC.presence_of_element_located((By.NAME, 'old_password')))
 
 
     def tearDown(self):
