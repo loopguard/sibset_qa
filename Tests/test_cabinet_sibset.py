4674f7915f46b2d3e13f9a45b630837a12042818
Alexander Efimov <fonwolfgang@gmail.com>
2017-08-04 13:32:16 +0700

    2do
    44 - 47 строки пофиксить

--- Tests/test_cabinet_sibset.py
+++ Tests/test_cabinet_sibset.py
@@ -7,7 +7,7 @@ http://cabinet.sibset.ru/admin/auth/login
 """
 
 from selenium import webdriver
-import unittest, time
+import unittest
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
@@ -20,9 +20,9 @@ class TestCabinetChrome(unittest.TestCase):
         self.driver.implicitly_wait(10)
         self.driver.get('http://dev.cabinet.sibset.ru/admin/account')
         self.driver.find_element_by_id(
-            'adminuser-login').send_keys('$') #login
+            'adminuser-login').send_keys('a.efimov') #login
         self.driver.find_element_by_id(
-            'adminuser-password').send_keys('$') #password
+            'adminuser-password').send_keys('Pewpew15') #password
         self.driver.find_element_by_class_name(
             'admin_submit_button').click()
         return self.driver
@@ -34,13 +34,17 @@ class TestCabinetChrome(unittest.TestCase):
         3.Проверяем что мы в личном кабинете
         """
         driver = self.driver
+        wait = WebDriverWait(driver, 10)
         contract = driver.find_element_by_id(
             'adminabonent-contract')
         submit = driver.find_element_by_class_name(
             'admin-contract-button')
         contract.send_keys('353829')
         submit.click()
-        driver.implicitly_wait(10)
+        wait.until(
+            EC.visibility_of(
+                By.CLASS_NAME(
+                    'cabinet_index_page')))
         self.assertIn(
             "Личный кабинет", driver.title)
 
